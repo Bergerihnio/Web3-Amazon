22206677679edf166fb2376d6da1e04e44f3818b
Bergerihnio <filipekbergerek@protonmail.com>
2024-09-03 21:47:53 +0200

    Connect to ABI

--- src/App.js
+++ src/App.js
@@ -15,13 +15,48 @@ import config from './config.json'
 function App() {  
   const [account, setAccount] = useState(null)
 
+  const [provider, setProvider] = useState(null)
 
+  const loadBlockchainData = async () => {
+    // connect to Blockchaiun
+    const provider = new ethers.providers.Web3Provider(window.ethereum)
+    setProvider(provider)
+
+    const network = await provider.getNetwork()
+    console.log(network)
+
+    // connect to smart contracts
+    const dappazon = new ethers.Contract(
+      config[network.chainId].dappazon.address,
+      Dappazon,
+      provider
+    )
+
+    // Load products
+
+    const items = []
+
+    for (let i = 0; i < 9; i++) {
+      const item = await dappazon.items(i + 1)
+      items.push(item)
+      // console.log(item) // single item
+    }
+
+    const electronics = items.filter((item) => item.category === 'electronics')
+    const clothing = items.filter((item) => item.category === 'clothing')
+    const toys = items.filter((item) => item.category === 'toys')
+   
+  }
+
+  useEffect(() => {
+    loadBlockchainData()
+  }, [])
 
   return (
     <div>
       <Navigation account={account} setAccount={setAccount}/>
 
-      <h2>Welcome to Dappazon!</h2>
+      <h2>Dappazon Best Sellers</h2>
 
     </div>
   );
